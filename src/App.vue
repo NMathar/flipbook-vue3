13c0246d6ee4294ab57588805af8a135d1dc3314
Константин <gladesinger18@gmail.com>
2024-12-16 22:45:42 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import FlipBook from '../lib/FlipBook.vue'
+import IconArrow from './components/IconArrow.vue'
+
+// Array of image paths
+const pages = ref([])
+</script>
+
+<template>
+  <div class="slider-wrap">
+    <FlipBook v-slot="flipbook" class="flipbook" :pages="pages" :gloss="0" :click-to-zoom="true" alt="Книжка">
+      <button aria-label="Предыдущий" class="flipbook-button button-prev" :class="{'disabled': !flipbook.canFlipLeft}" @click="flipbook.flipLeft">
+        <IconArrow />
+      </button>
+      <button aria-label="Следующий" class="flipbook-button button-next" :class="{'disabled': !flipbook.canFlipRight}" @click="flipbook.flipRight">
+        <IconArrow />
+      </button>
+    </FlipBook>
+  </div>
+</template>
+
+<style scoped>
+.slider-wrap {
+  position: relative;
+  width: 100vw;
+  max-width: 1120px;
+  margin: 0 auto;
+}
+
+.slider-wrap .flipbook {
+  width: 100%;
+  height: 410px;
+  display: block;
+}
+
+.slider-wrap .flipbook img {
+  max-width: 100%;
+}
+
+.slider-wrap .flipbook-button {
+  position: absolute;
+  top: 50%;
+  cursor: pointer;
+  background: none;
+  border: none;
+  padding: 0;
+  outline: none;
+}
+
+.slider-wrap .flipbook-button.button-prev {
+  left: -64px;
+  transform: translateY(-50%);
+}
+.slider-wrap .flipbook-button.button-next {
+  right: -64px;
+  transform: rotate(180deg) translateY(50%);
+}
+.slider-wrap .flipbook-button.disabled {
+  opacity: 0.35;
+  cursor: auto;
+  pointer-events: none;
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      name: 'FlipbookVue3',
+      // the proper extensions will be added
+      fileName: 'flipbook-vue3'
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
+})
