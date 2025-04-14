78fa48f3309d7ae980ec9ca0613d167bc1c00c5a
nm254744 <nicolai.mathar@ima.rwth-aachen.de>
2025-04-14 13:28:09 +0200

    add module declaration

---
+++ src/types/flipbook-vue3.d.ts
@@ -0,0 +1,29 @@
+declare module '@nmathar/flipbook-vue3' {
+	import { DefineComponent } from 'vue';
+
+	export interface FlipBookProps {
+		pages: string[];
+		pagesHiRes?: string[];
+		flipDuration?: number;
+		zoomDuration?: number;
+		zooms?: number[];
+		swipeMin?: number;
+		singlePage?: boolean;
+		forwardDirection?: 'right' | 'left';
+		centering?: boolean;
+		startPage?: number;
+		clickToZoom?: boolean;
+		dragToFlip?: boolean;
+		wheel?: 'scroll' | 'flip';
+		alt?: string;
+		loadingImage?: string;
+		loadingImageHiRes?: string;
+		perspective?: number;
+		nPolygons?: number;
+		ambient?: number;
+		gloss?: number;
+	}
+
+	const component: DefineComponent<FlipBookProps, {}, any>;
+	export default component;
+}
